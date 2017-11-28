66fdb18fd511ed8ed831d8b6336d55d5d4d49df1
ikorgik <ikorgik@gmail.com>
2017-11-28 20:04:10 +0300

    Updated images in code.

--- frontend-gifts/src/components/GlobalHeader/Logo.js
+++ frontend-gifts/src/components/GlobalHeader/Logo.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const Logo = () => (
   <span>
     <Link to="/" className="site-logo visible-desktop">
-      <img src="/images/logo.gif" alt="Gifts" />
+      <img src="/images/logo.png" alt="Gifts" />
     </Link>
     <Link to="/" className="site-logo visible-mobile">
-      <img src="/images/logo.gif" alt="Gifts" />
+      <img src="/images/logo.png" alt="Gifts" />
     </Link>
   </span>
 );
