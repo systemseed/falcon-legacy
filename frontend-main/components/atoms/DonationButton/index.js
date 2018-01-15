b5febe256a37ecc925ea8958ed2641524b64d442
ikorgik <ikorgik@gmail.com>
2018-01-15 21:49:26 +0300

    [#153221936] Added DonationButton component.

---
+++ frontend-main/components/atoms/DonationButton/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const DonationButton = ({ type, size, block, active, disabled, onClick, children }) => {
+  let classes = ['btn'];
+  if (type) {
+    classes.push('btn-' + type);
+  }
+
+  if (size) {
+    classes.push('btn-' + size);
+  }
+
+  if (block) {
+    classes.push('btn-block');
+  }
+
+  if (active) {
+    classes.push('btn-active');
+  }
+
+  if (disabled) {
+    classes.push('btn-disabled');
+  }
+
+  return (
+    <button type="button" onClick={onClick} className={classes.join(' ')}>
+      {children}
+    </button>
+  );
+};
+
+DonationButton.propTypes = {
+  donationUrl: PropTypes.string,
+  type: PropTypes.oneOf(['white', 'pink']),
+  textSize: PropTypes.oneOf(['lg', 'md', 'sm', 'xs']),
+  block: PropTypes.bool,
+  active: PropTypes.bool,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  children: PropTypes.node,
+};
+
+DonationButton.defaultProps = {
+  type: 'pink',
+  textSize: 'md',
+  block: false,
+  active: false,
+  disabled: false,
+  onClick: () => {},
+};
+
+export default DonationButton;
