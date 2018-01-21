050a98379701489bc391bdb46ecb4a36b163d5d1
ikorgik <ikorgik@gmail.com>
2018-01-21 19:38:24 +0300

    [#153221936] Fixed class in donation button component.

--- frontend-main/components/atoms/DonationButton/index.js
+++ frontend-main/components/atoms/DonationButton/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 
-export default ({ children, ...attributes }) => {
-  let className = "cw-donate-button";
-  if (attributes.className !== undefined) {
-    className += " " + attributes.className;
+export default ({ children, className, ...attributes }) => {
+  let buttonClass = "cw-donate-button";
+  if (className !== undefined) {
+    buttonClass += " " + className;
   }
 
   return (
-    <Button className={className} {...attributes}>{children}</Button>
+    <Button className={buttonClass} {...attributes}>{children}</Button>
   )
 }
