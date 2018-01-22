9b7767d34c6cc9145e4d29732a4a239bd12d3463
ikorgik <ikorgik@gmail.com>
2018-01-22 13:26:33 +0300

    [#153222598] Updated env path.

--- frontend-main/application/HtmlHead.js
+++ frontend-main/application/HtmlHead.js
@@ -10,7 +10,7 @@ const HtmlHead = ({ metaData, analytics, favicon }) => {
   let stylesheets;
   if (process.env.NODE_ENV === 'production') {
     // In production, serve pre-built CSS file from /assets/{version}/main.css
-    const pathToCSS = `/assets/${Package.version}/main.css`;
+    const pathToCSS = `${process.env.BASE_URL}/assets/${Package.version}/main.css`;
     stylesheets = <link rel="stylesheet" type="text/css" href={pathToCSS} />;
   }
   else {
@@ -28,7 +28,7 @@ const HtmlHead = ({ metaData, analytics, favicon }) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        {!!favicon && <link rel="shortcut icon" href={favicon} type="image/vnd.microsoft.icon" />}
+        {!!favicon && <link rel="shortcut icon" href={process.env.BASE_URL + favicon} type="image/vnd.microsoft.icon" />}
         {metaData.description && <meta name="description" content={metaData.description} />}
         {metaData.keywords && <meta name="keywords" content={metaData.keywords} />}
         {stylesheets}
--- frontend-main/pages/about.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default () =>
-  <div>
-    <h1>Hello from frontend-main/pages/about.js!</h1>
-    <img src={`${process.env.BASE_URL}/static/image.jpeg`} width="100%" />
-  </div>
