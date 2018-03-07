b77fe35a86f68508700f932a1cc7bdff51de9795
ikorgik <ikorgik@gmail.com>
2018-03-07 12:26:53 +0300

    [#154847964] Updated footer data component.

--- frontend-main/components/organisms/FooterDataPane/index.js
+++ frontend-main/components/organisms/FooterDataPane/index.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 import FooterData from '../../molecules/FooterData';
 
 const FooterDataPane = ({ styles, copy, imageUrl }) => {
+  if (!copy) {
+    return null;
+  }
   return (
     <div className={"row justify-content-center limited-width registration-data-pane " + styles}>
       <div className="col-12 col-md-8 col-xl-10">
-        <FooterData copy={copy} imageUrl={imageUrl} />
+        <FooterData copy={copy} imageUrl={imageUrl}/>
       </div>
     </div>
   );
