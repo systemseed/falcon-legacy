9d2b98b374d2af90d5af5b3c72873ebb702a0fc8
kalabro <kalabro@ya.ru>
2018-03-19 15:46:11 +0300

    Added a post-tests hook to push master to child repositiries.

--- .circleci/scripts/falcon/post-tests-hook.sh
+++ .circleci/scripts/falcon/post-tests-hook.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
-# Environment variables (set these via CircleCI GUI):
-# FALCON_CI_TO_TRIGGER_TOKEN - token from CircleCI to use API. Can be generate in 'API Permissions' section of CircleCI settings.
-# FALCON_CI_TO_TRIGGER_USERNAME - the GitHub or Bitbucket account username used by CircleCI build.
-# FALCON_CI_TO_TRIGGER_PROJECT - the GitHub or Bitbucket project name used by CircleCI build.
 
-# Trigger other CircleCI build if all tests pass.
-# See CircleCI documentation for more information: https://circleci.com/docs/api/v1-reference/#new-build-branch
-if [ -v FALCON_BUILD_TO_TRIGGER_CI_TOKEN ] && [ -v FALCON_CI_TO_TRIGGER_USERNAME ] && [ -v FALCON_CI_TO_TRIGGER_PROJECT ] && [ $CIRCLE_BRANCH != "master" ] && [ $CIRCLE_BRANCH != "stage" ]; then
-  curl -X POST https://circleci.com/api/v1.1/project/github/${FALCON_CI_TO_TRIGGER_USERNAME}/${FALCON_CI_TO_TRIGGER_PROJECT}/tree/master?circle-token=${FALCON_BUILD_TO_TRIGGER_CI_TOKEN}
+# Script to push Falcon's master to client repositories.
+# Triggered by Circle CI.
+if [ $CIRCLE_BRANCH != "master" ]; then
+  exit 0
 fi
+
+git config --global user.email "circleci@systemseed.com"
+git config --global user.name "Falcon Circle CI"
+
+# Find all variables set in CIRCLE CI starting with CLIENT_*.
+vars=$(env | awk -F '=' '{print $1}' | grep 'CLIENT_*') || true
+
+# Create a new variable $client_repos which is now array of env vars
+# starting with CLIENT_*.
+read -a client_repos <<<$vars
+
+# Every CLIENT_* variable should define a remote git repo.
+# So we loop through client repos and deploy changes to each of those.
+for repo_var in "${client_repos[@]}"
+do
+  # Here ${repo_var} is name of env variable, ${!repo_var} is git remote url.
+  echo "Pushing changes to ${!repo_var} (${repo_var})..."
+  if ! git config remote.${repo_var}.url > /dev/null; then
+    git remote add ${repo_var} ${!repo_var}
+  fi;
+
+  # Fetch latest version of "falcon" branch from client repository.
+  git fetch cw2ie falcon
+
+  # Create new temporary branch on top of "falcon".
+  git checkout -B ${repo_var}-falcon ${repo_var}/falcon
+
+  # Merge Falcon's $CIRCLE_BRANCH into the new branch.
+  git merge --no-edit $CIRCLE_BRANCH
+
+  echo "push --force ${repo_var} ${repo_var}-falcon:falcon"
+  # Uncomment after initial testing.
+  #git push --force ${repo_var} ${repo_var}-falcon:falcon
+
+  # Get back to the original branch.
+  git checkout $CIRCLE_BRANCH
+done
