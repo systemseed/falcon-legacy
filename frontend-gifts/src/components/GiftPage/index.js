878b9dbea55bcefeb30e61b16c4edc679d87e043
otarza <otar.zakalashvili@gmail.com>
2018-06-13 20:06:53 +0400

    [#155003574] Implemented tiles like sections for Gift page.

--- frontend-gifts/src/components/GiftPage/index.js
+++ frontend-gifts/src/components/GiftPage/index.js
@@ -2,19 +2,46 @@ import React from 'react';
 import GiftHeroImage from './GiftHeroImage';
 import GiftBody from './GiftBody';
 import GiftTabs from './GiftTabs';
+import * as productUtils from '../../utils/products';
 
 const GiftPage = ({ gift, currentCurrency }) => (
-  <div className="container">
+  <div className="container gift-container gutters-md no-gutters">
     <div className="bg-white">
-      <div className="row">
-        <div className="col-md-6">
+
+      {/* primary content */}
+      <div className="row row-eq-height">
+        <div className="col-sm-6 gutters-md no-right-gutter-md no-gutters">
           <GiftHeroImage gift={gift}/>
         </div>
-        <div className="col-md-6">
+        <div className="col-sm-6 gutters-md no-gutters">
           <GiftBody gift={gift} currentCurrency={currentCurrency}/>
         </div>
       </div>
-      <GiftTabs gift={gift}/>
+      {/* end primary content */}
+
+      {/* gift in action */}
+      <div className="row row-eq-height">
+        <div className="col-sm-6 col-sm-push-6 gutters-md no-left-gutter-md no-gutters">
+          <section className="fw-section gift-primary-image-container object-fit-wrapper">
+            <img className="object-fit-cover" src={gift.actionImageUrl} alt={gift.actionImageAlt}
+                 title={gift.actionImageAlt}/>
+          </section>
+          {/*<GiftHeroImage gift={gift} />*/}
+        </div>
+        <div className="col-sm-6 col-sm-pull-6 gutters-md no-gutters">
+          <section className="fw-section">
+            <div className="gift-content secondary-section-container">
+              <h1 className="gift-title secondary-section-title">Gift in action</h1>
+              {gift.actionDescription &&
+              <div className="padding-right" dangerouslySetInnerHTML={{ __html: gift.actionDescription }}/>
+              }
+            </div>
+          </section>
+        </div>
+      </div>
+      {/* end gift in action */}
+
+      {/*<GiftTabs gift={gift}/> */}
     </div>
   </div>
 );
